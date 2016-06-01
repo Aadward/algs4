54fbc41d860bdf697204f5a4b6db09aa1ef9c0ad
syh <512563551@qq.com>
2016-06-01 13:59:32 +0800

    DP LPS

--- src/sundry/LPS.java
+++ src/sundry/LPS.java
@@ -9,8 +9,25 @@ public class LPS {
 		return Math.max(lps(s,head + 1, tail),lps(s,head, tail - 1));
 	}
 	
+	public static int dplps(char[] s){
+		int[][] dp = new int[s.length][s.length];
+		for(int i = 0; i < s.length; i++)  dp[i][i] = 1;
+		for(int i = 1; i < s.length; i++){
+			for(int j = 0; i + j < s.length; j++){
+				int temp = 0;
+				if(s[j] == s[j + i])
+					temp = dp[j+1][j+i-1] + 2;
+				else
+					temp = Math.max(dp[j+1][j+i],dp[j][j+i-1]);
+				dp[j][j+i] = temp;
+			}
+		}
+		return dp[0][s.length-1];
+	}
+	
 	public static void main(String[] args) {
 		char[] c = "google".toCharArray();
 		System.out.println(lps(c,0,c.length - 1));
+		System.out.println(dplps(c));
 	}
 }
